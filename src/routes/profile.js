d55081266d8290e53af42a40e1cbccde4edd7870
Abhishek Tripathi <abhishektripathi8582@gmail.com>
2025-10-07 20:31:44 +0530

    Almost Finished

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -9,6 +9,72 @@ profileRouter.get("/profile", userAuth, async (req, res) => {
   res.send(req.user);
 });
 
+profileRouter.get("/profile/:random/:type", userAuth, async (req, res) => {
+  try {
+    const { type, random } = req.params;
+    console.log(req.user);
+    const { _id } = req.user;
+    const allowedType = ["remaining", "mastered"];
+    if (!allowedType.includes(type)) {
+      throw new Error("There's no such type exist");
+    }
+    if (type === "remaining") {
+      const data = await RemainingModel.find({ uid: _id }).populate("qid");
+      if (random === "true") {
+        const randomItem = data[Math.floor(Math.random() * data.length)];
+        res.json({ data: randomItem });
+      } else {
+        res.json({ data });
+      }
+    } else {
+      const data = await MasteredModel.find({ uid: _id }).populate("qid");
+      if (random === "true") {
+        const randomItem = data[Math.floor(Math.random() * data.length)];
+        res.json({ data: randomItem });
+      } else {
+        res.json({ data });
+      }
+    }
+  } catch (e) {
+    res.json("Got the error " + e.message);
+  }
+});
+
+profileRouter.post(
+  "/profile/editDesc/:type/:_id",
+  userAuth,
+  async (req, res) => {
+    try {
+      const { type, _id } = req.params;
+      const allowedType = ["remaining", "mastered"];
+      if (!allowedType.includes(type)) {
+        throw new Error("There's no such type exist");
+      }
+      if (type === "remaining") {
+        const { curDesc } = req.body;
+        const data = await RemainingModel.findById(_id);
+        if (data == null) {
+          throw new Error("No data found");
+        }
+        data.desc = curDesc;
+        await data.save();
+        res.json({ message: "Successfully edited the description !" });
+      } else {
+        const { curDesc } = req.body;
+        const data = await MasteredModel.findById(_id);
+        if (data == null) {
+          throw new Error("No data found");
+        }
+        data.desc = curDesc;
+        await data.save();
+        res.json({ message: "Successfully edited the description !" });
+      }
+    } catch (e) {
+      res.json({ message: "Here's the error " + e.message });
+    }
+  }
+);
+
 profileRouter.post("/profile/:type/:_id", userAuth, async (req, res) => {
   try {
     const { type, _id } = req.params;
@@ -52,28 +118,6 @@ profileRouter.post("/profile/:type/:_id", userAuth, async (req, res) => {
     }
   } catch (e) {
     res.json({ message: "there was an error " + e.message });
-  }
-});
-
-profileRouter.get("/profile/:type", userAuth, async (req, res) => {
-  try {
-    console.log("hiii");
-    const { type } = req.params;
-    console.log(req.user);
-    const { _id } = req.user;
-    const allowedType = ["remaining", "mastered"];
-    if (!allowedType.includes(type)) {
-      throw new Error("There's no such type exist");
-    }
-    if (type === "remaining") {
-      const data = await RemainingModel.find({ uid: _id }).populate("qid");
-      res.json({ data });
-    } else {
-      const data = await MasteredModel.find({ uid: _id }).populate("qid");
-      res.json({ data });
-    }
-  } catch (e) {
-    res.status(400).json({ message: "Error " + e.message });
   }
 });
 
